9f171d48167e917e78db15777610891aa480e454
Debora <escobardebo@gmail.com>
2024-01-06 18:36:25 -0300

    Dashboard terminado

---
+++ src/components/ContentRowTop.jsx
@@ -0,0 +1,24 @@
+import { ContentRowMovies } from "./ContentRowMovies"
+import { LastMovieInDB } from "./LastMovieInDB"
+import { GenresInDB } from "./GenresInDB"
+
+export const ContentRowTop = () => {
+  return (
+    <div className="container-fluid">
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+            </div>
+        
+
+          <ContentRowMovies/>
+
+    
+            <div className="row">
+        
+             <LastMovieInDB/>
+            
+              <GenresInDB/>
+            </div>
+        </div>
+  )
+}
